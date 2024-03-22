a1103152f00a7eed66d00d68b374d843ad333ba1
omerfaruksunar <omerfaruksunar@gmail.com>
2024-03-22 17:21:40 +0300

    Update ICommonNoKeyRepository.cs

--- src/AbcYazilim.OnMuhasebe.Domain/Commons/ICommonNoKeyRepository.cs
+++ src/AbcYazilim.OnMuhasebe.Domain/Commons/ICommonNoKeyRepository.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
 
 namespace AbcYazilim.OnMuhasebe.Commons;
-internal interface ICommonNoKeyRepository
+
+//Id zorunlulugu olmadan ekleme yapmak icin.
+public interface ICommonNoKeyRepository<TEntity>:IRepository<TEntity>
+where TEntity : class, IEntity
 {
+	Task<TEntity> FromSqlRawSingleAsync(string sql,params object[] parameters);
+	Task<IList<TEntity>> FromSqlRawAsync(string sql, params object[] parameters);
 }
