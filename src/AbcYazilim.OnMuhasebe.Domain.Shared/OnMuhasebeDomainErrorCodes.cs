6f3510a3d1bfc87f888076c177f66a0bde8d5cfb
omerfaruksunar <omerfaruksunar@gmail.com>
2024-05-03 18:11:09 +0300

    Create BankaHesapEntityValidator Class

--- src/AbcYazilim.OnMuhasebe.Domain.Shared/OnMuhasebeDomainErrorCodes.cs
+++ src/AbcYazilim.OnMuhasebe.Domain.Shared/OnMuhasebeDomainErrorCodes.cs
@@ -5,4 +5,6 @@ public static class OnMuhasebeDomainErrorCodes
 	/* You can add your business exception error codes here, as constants */
 	public const string DuplicateKod = "Exception:00001";
 	public const string CannotBeDeleted = "Exception:00002";
+	public const string Required = "Exception:00003";
+	public const string MaxLength = "Exception:00004";
 }
